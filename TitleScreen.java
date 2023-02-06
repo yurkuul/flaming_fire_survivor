f3bb698388fa9f5ddc30447d8f6c2f2de846c0bd
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-02-05 23:01:51 -0600

    Create TitleScreen.java

---
+++ TitleScreen.java
@@ -0,0 +1,34 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
+
+public class TitleScreen extends World
+{
+    private boolean scenarioStarted;
+
+    public TitleScreen() {    
+        super(GameConstants.WORLD_W, GameConstants.WORLD_H, 1);
+        scenarioStarted = false;
+        setBackground ("title_bg.png");
+        prepare();
+    }
+
+    private void prepare(){
+         
+    }
+
+    public void act() {
+        
+    }
+
+    private void addCloud() {
+        
+    }
+
+    public void startMusic() {
+        
+    }
+
+    public void pauseMusic() {
+        
+    }
+}
