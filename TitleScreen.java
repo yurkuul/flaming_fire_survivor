4a244980dbe25ee9d075867eaa52878fba476e4d
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-02-07 09:42:11 -0600

    Update TitleScreen.java

--- TitleScreen.java
+++ TitleScreen.java
@@ -12,8 +12,21 @@ public class TitleScreen extends World
         prepare();
     }
 
-    private void prepare(){
-         
+    private void prepare() {
+        //Adds in 50 stars for flashiness :D
+        for (int i = 0; i < 50; i++) {
+            addObject (new Star(), Greenfoot.getRandomNumber (GameConstants.WORLD_W), Greenfoot.getRandomNumber (300));
+        }
+        //Text Titles that take on parameters to determine the button type
+        //Start Button
+        addObject (new TitleText(1), 1105, 374);
+        //Instructions Button
+        addObject (new TitleText(2), 1000, 450);
+        //Adds in 2 clouds at the beginning
+        Cloud cloud = new Cloud(1);
+        addObject(cloud,1293,98);
+        Cloud cloud2 = new Cloud(0);
+        addObject(cloud2,2,279);
     }
 
     public void act() {
@@ -25,10 +38,10 @@ public class TitleScreen extends World
     }
 
     public void startMusic() {
-        
+        GameConstants.music.playLoop();
     }
 
     public void pauseMusic() {
-        
+        GameConstants.music.pause();
     }
 }
