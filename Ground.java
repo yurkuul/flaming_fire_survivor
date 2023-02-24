7b3c0f3b65daa0b726e2d2d64d8fcf0cd2d3ac6e
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-02-24 11:38:21 -0600

    Create Ground.java

---
+++ Ground.java
@@ -0,0 +1,11 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The ground used in the Game world.
+ */
+public class Ground extends Actor
+{
+      public Ground() {
+          
+      }
+}
