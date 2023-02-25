330f7570830311a4dc47fcc928f5f1f96f0fdaec
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2023-02-25 15:23:52 -0600

    Create Ladder.java

---
+++ Ladder.java
@@ -0,0 +1,10 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Ladders used in the Game world which the player can climb up and down on
+ */
+public class Ladder extends Actor {
+    public Ladder() {
+
+    }
+}
